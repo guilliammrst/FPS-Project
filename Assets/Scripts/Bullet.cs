4c98878241b8fb91bfd2d9719aa0386d67bcabc4
Clément KERVICHE <ckerviche@myges.fr>
2024-04-02 18:30:58 +0200

    V0.3

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,12 +4,28 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public int damage = 10;
+
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.CompareTag("Target"))
+        if (collision.gameObject.CompareTag("Enemy"))
+        {
+            EnemyHealth enemyHealth = collision.gameObject.GetComponent<EnemyHealth>();
+
+            if (enemyHealth != null)
+            {
+                enemyHealth.TakeDamage(damage);
+
+                print("Enemy HP: " + enemyHealth.currentHealth);
+
+                Destroy(gameObject);
+            }
+        }
+        else if (collision.gameObject.CompareTag("Target"))
         {
             print("hit " + collision.gameObject.name + "!");
             Destroy(gameObject);
         }
     }
 }
+
