fc386ac1f486d15bae639f47d2bcc957e20595b3
guilliammrst <guilliam.morisset85@gmail.com>
2024-06-20 11:06:43 +0200

    Fix ennemies stuck by walls

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -19,7 +20,7 @@ public class Bullet : MonoBehaviour
     {
         GameObject hit = objectWeHit.gameObject;
 
-        if (hit.name.Contains(shooter))
+        if (hit.name.Contains(shooter) && shooter != null && shooter != "")
         {
             return;
         }
